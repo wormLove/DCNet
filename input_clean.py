048babf557db6fa225660d1a6901fffd700c8e32
rraj126 <rishabh.bsbe@gmail.com>
2023-04-24 19:55:08 -0500

    processed repeated labels

--- input_clean.py
+++ input_clean.py
@@ -3,13 +3,11 @@ import numpy as np
 def set_data_range(data: np.ndarray):
     min_zero_data = data - np.min(data, axis = 0)
     data_p = min_zero_data / np.max(min_zero_data, axis = 0)
-    
     return data_p
 
 def remove_zero_cols(data: np.ndarray):
     good_index = np.sum(data, axis = 0) != 0
     data_p = data[:, good_index]
-    
     return data_p
 
 def remove_nans(data: np.ndarray):
--- input_file.py
+++ input_file.py
@@ -3,26 +3,27 @@ import numpy as np
 from typing import List
 import warnings
 
+def process_labels(data_labels: List[str]):
+    processed_labels = [s.split('.')[0] for s in data_labels]
+    return processed_labels
+
+
 def get_sheet_data(f: dict, sheet: str):
     df = f[sheet]
-
     data_as_array = df.to_numpy()
     data_size = data_as_array.shape
-    
     return data_as_array, data_size
 
 def get_sheet_labels(f: dict, sheet: str, ncols: int, header: bool):
     df = f[sheet]
-    
     if header:
-        data_labels = [str(value) for value in list(df.columns.values)]
+        data_labels_raw = [str(value) for value in list(df.columns.values)]
+        data_labels = process_labels(data_labels_raw)
         if len(data_labels) != ncols:
             raise Exception("improper labels")
     else:
         data_labels = ['<ukn>']*ncols
-
     return data_labels
-        
 
 def read_sheet(f: dict, sheet: str, header: bool = True):
     sheet_dict = {}
@@ -31,16 +32,12 @@ def read_sheet(f: dict, sheet: str, header: bool = True):
     sheet_dict['values'] = data_values
     sheet_dict['size'] = data_size
     sheet_dict['labels'] = get_sheet_labels(f, sheet, int(data_size[1]), header)
-
     return sheet_dict
 
 def read_data(file_path: str, header: bool = True, sheets: List[str] = []):
     f = pd.read_excel(file_path, None)
     sheet_names_from_file = [str(key) for key in f.keys()]
-    
-    
     data_dict = {}
-    
     if sheets:
         for sheet in sheets:
             if sheet in sheet_names_from_file:
@@ -50,5 +47,4 @@ def read_data(file_path: str, header: bool = True, sheets: List[str] = []):
     else:
         for sheet in sheet_names_from_file:
             data_dict[sheet] = read_sheet(f, sheet)
-        
     return data_dict
